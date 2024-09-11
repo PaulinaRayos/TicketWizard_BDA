c4fcbd61a51b1e6e632db43114863320d43072e5
PaulinaRayos <163494315+PaulinaRayos@users.noreply.github.com>
2024-09-10 22:57:39 -0700

    Se agrego el Trigger

--- Script_Ticketwizard.sql
+++ Script_Ticketwizard.sql
@@ -1,9 +1,8 @@
-
 CREATE DATABASE ticketwizard;
 
 use ticketwizard;
 
-CREATE TABLE Usuario (
+CREATE TABLE Usuarios (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nombre_completo VARCHAR(255) NOT NULL,
     correo VARCHAR(100) UNIQUE NOT NULL,
@@ -14,16 +13,16 @@ CREATE TABLE Usuario (
 )AUTO_INCREMENT = 1;
 
 
-CREATE TABLE Domicilio (
+CREATE TABLE Domicilios (
     id_domicilio INT AUTO_INCREMENT PRIMARY KEY,
     calle VARCHAR(255),
     numero_exterior VARCHAR(10),
     colonia VARCHAR(100),
     id_usuario INT,
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
+    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
 )AUTO_INCREMENT = 1;
 
-CREATE TABLE Evento (
+CREATE TABLE Eventos (
     id_evento INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     fecha DATE NOT NULL,
@@ -32,7 +31,7 @@ CREATE TABLE Evento (
     venue VARCHAR(255) NOT NULL
 )AUTO_INCREMENT = 1;
 
-CREATE TABLE Boleto (
+CREATE TABLE Boletos (
     id_boleto INT AUTO_INCREMENT PRIMARY KEY,
     num_serie CHAR(8) UNIQUE NOT NULL,
     fila VARCHAR(10),
@@ -41,18 +40,31 @@ CREATE TABLE Boleto (
     estado_adquisicion ENUM('reventa', 'directo') NOT NULL,
     id_usuario INT, 
     id_evento INT, 
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
-    FOREIGN KEY (id_evento) REFERENCES Evento(id_evento)
+    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
+    FOREIGN KEY (id_evento) REFERENCES Eventos(id_evento)
 )AUTO_INCREMENT = 1;
 
-CREATE TABLE Transaccion (
+CREATE TABLE Transacciones (
     id_transaccion INT AUTO_INCREMENT PRIMARY KEY,
     num_transaccion VARCHAR(20) UNIQUE NOT NULL,
     monto DECIMAL(10, 2) NOT NULL,
     fecha_hora_adquisicion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     id_usuario INT,
     id_boleto INT, 
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
-    FOREIGN KEY (id_boleto) REFERENCES Boleto(id_boleto)
+    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
+    FOREIGN KEY (id_boleto) REFERENCES Boletos(id_boleto)
 )AUTO_INCREMENT = 1;
+
+DELIMITER //
+
+CREATE TRIGGER actualizar_usuario_saldo
+AFTER INSERT ON Transacciones
+FOR EACH ROW
+BEGIN
+    UPDATE Usuarios
+    SET saldo = saldo + NEW.monto
+    WHERE id_usuario = NEW.id_usuario;
+END //
+
+DELIMITER ;
 
